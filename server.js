c6c186bf1c17749d1d1dc1f344c3d00b82bcb446
Douglas T. Angram <douglast.angram@douglass-mbp.attlocal.net>
2020-04-26 18:37:24 -0500

    Heroku Launch

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Heroku Staging
 // Serve any static files
-app.use(express.static(path.join(__dirname, 'reactjs/build')));
+app.use(express.static(path.join(__dirname, 'wd6-motoshop/build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'reactjs/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'wd6-motoshop/build', 'index.html'));
 });
 
 // Heroku Production
